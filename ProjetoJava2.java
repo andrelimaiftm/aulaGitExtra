6c1ee062a49ae8ceb31c455b6627b7cd2c1fa2f7
Andre Chaves <andrelima@iftm.edu.br>
2022-03-21 10:34:36 -0300

    implementação programa soma dois numeros.

--- ProjetoJava2.java
+++ ProjetoJava2.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class ProjetoJava2 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
+        int numero1 = entrada.nextInt();
+        System.out.print("Digite outro numero: ");
+        int numero2 = entrada.nextInt();
+        int total = numero1+numero2;
+        System.out.println("Resultado: " + total);
+        entrada.close();
+    }
+
+    
     
 }
